cd38390f0a2278853dbe9285f86a12c71596ef26
Ower <owerwild@gmail.com>
2023-06-17 22:20:53 +0200

    Theater OK

--- pages/components/CartonTheater.old.jsx
+++ pages/components/CartonTheater.old.jsx
@@ -5,18 +5,29 @@ import Image from 'next/image'
 import SectionTitle from './SectionTitle'
 
 export default function CartonTheater() {
+
+
   const [containerHeight, setContainerHeight] = useState(null)
 
   const bgref = useRef(null)
+  
+  useEffect(() => {
+    if (bgref.current?.clientHeight) {
+      setContainerHeight(bgref.current?.clientHeight + 50)
+    }
+    
+  }, [bgref ])
+
 
   return (
-    <div className={styles.container}>
-      <div className={styles.background}>
-        <Image src={theaterBackground} alt="cartonTheaterBg" />
-      </div>
-      <div className={styles.content}>
-        <SectionTitle title="Theater" />
-      </div>
+   <div className={styles.container}  style={{ minHeight: containerHeight }}>
+    <div className={styles.background}>
+      <Image src={theaterBackground} alt='cartonTheaterBg' ref={bgref} />
     </div>
+    <div className={styles.content}>
+      <SectionTitle title='Theater' />
+    </div>
+
+   </div>
   )
 }
